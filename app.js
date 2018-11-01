1b4cc2c2107708cec48fdeae46d1873e2997c65a
EC2 Default User <ec2-user@ip-172-31-21-37.us-east-2.compute.internal>
2018-11-01 01:27:42 +0000

    implemented initial chat framework

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+
+var history = [];
+
+
+//set the template engine ejs
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(express.static('public'));
+
+
+//routes
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+//Listen on port 4200
+var server = app.listen(4200);
+
+
+
+//socket.io instantiation
+const io = require("socket.io")(server);
+
+
+//listen on every connection
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+	//default username
+	socket.username = "Anonymous";
+
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username;
+    });
+
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+        history.push({username : socket.username, message : data.message});
+        console.log(socket.username);
+        console.log(data.message);
+        console.log(history.length);
+        for (var i = 0; i < history.length; ++i){
+            console.log('Username: ' + history[i].username + '-- Message: ' + history[i].message);
+        }
+    });
+
+    //listen on typing
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username});
+    });
+    
+    socket.on('load_history', () => {
+        socket.emit('load_history', history);
+    })
+});
